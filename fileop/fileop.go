875deb4c66dc369dd98acd1ec8f9d74faf0f8514
Jaya P Shrivastava <jaya@MacBook-Pro.local>
2017-10-12 00:42:43 +0530

    Proj1 modified

--- fileop/fileop.go
+++ fileop/fileop.go
@@ -1,68 +1,66 @@
 package fileop
 
 import (
-	"compress/gzip"
+  "compress/gzip"
   "crypto/md5"
   "io/ioutil"
   "bufio"
-	"bytes"
+  "bytes"
   "fmt"
   "io"
   "os"
 )
 
 func Checksum(filename string) ([]byte, error) {
-
   file, err := os.Open(filename)
   if err != nil {
-			fmt.Println(err)
-	}
-	defer file.Close()
-
-	hash := md5.New()
-	nBytes, err := io.Copy(hash,file)
-	if err != nil {
-		return nil, io.ErrUnexpectedEOF
+    fmt.Println(err)
   }
-  fmt.Printf("No.of bytes: %d\n", nBytes)
+  defer file.Close()
+
+  hash := md5.New()
+  _, err := io.Copy(hash,file)
+  if err != nil {
+    return nil, io.ErrUnexpectedEOF
+  }
+
   sum := hash.Sum(nil)
   fmt.Printf("Md5 Checksum: %x\n", sum)
-	return sum, nil
+  return sum, nil
 }
 
 func Compress(filename string) ([]byte, error) {
-
   file, err := os.Open(filename)
   if err != nil {
-			fmt.Println(err)
-	}
-	defer file.Close()
+    fmt.Println(err)
+  }
+  defer file.Close()
 
-	fileInfo, err := file.Stat()
-	size := fileInfo.Size()
+  fileInfo, err := file.Stat()
+  size := fileInfo.Size()
   gzf := filename+".gz"
 
-	//Read file data into buffer
-	nBytes := make([]byte, size)
-	buffer := bufio.NewReader(file)
-	_, err = buffer.Read(nBytes)
-	if err != nil {
-		fmt.Printf("Failed to read file :%s into buffer and error: %s\n",filename,err)
-		return nil, err
-	}
+  //Read file data into buffer
+  nBytes := make([]byte, size)
+  buffer := bufio.NewReader(file)
+  _, err = buffer.Read(nBytes)
+  if err != nil {
+    fmt.Printf("Failed to read file :%s into buffer and error: %s\n", filename, err)
+    return nil, err
+  }
 
-	//Create a gzip writer on top of file writer
-	var buf bytes.Buffer
-	gzw := gzip.NewWriter(&buf)
-	gzw.Write(nBytes)
+  //Create a gzip writer on top of file writer
+  var buf bytes.Buffer
+  gzw := gzip.NewWriter(&buf)
+  gzw.Write(nBytes)
   gzw.Close()
 
-	err = ioutil.WriteFile(gzf, buf.Bytes(), 06444)
+  err = ioutil.WriteFile(gzf, buf.Bytes(), 06444)
   if err != nil {
-		fmt.Printf("Failed to write to file :%s and error: %s\n",gzf,err)
-		return nil, err
-	}
-	fmt.Printf("Compressed file: %s with bytes:%d\n", gzf, buf.Bytes)
-	return buf.Bytes(), nil
+    fmt.Printf("Failed to write to file :%s and error: %s\n", gzf, err)
+    return nil, err
+  }
+  fmt.Printf("Compressed file: %s with bytes:%d\n", gzf, buf.Bytes)
+  return buf.Bytes(), nil
 }
 
--- proj1/checksum.go
+++ proj1/checksum.go
@@ -4,32 +4,30 @@ import (
   "flag"
   "fmt"
   "os"
-	"github.com/jayapsrivastava/golearning/fileop"
-
+  "github.com/jayapsrivastava/golearning/fileop"
 )
 
 func main() {
 
   flag.Parse()
-
   filename := flag.Arg(0)
 
   if filename == "" {
-			fmt.Println("Usage: go run checksum.go <file_to_read>")
-			os.Exit(1)
-	}
+    fmt.Println("Usage: go run checksum.go <file_to_read>")
+    os.Exit(1)
+  }
 
-	//Calculate checksum
+  //Calculate checksum
   sum, err := fileop.Checksum(filename)
   if err != nil {
-			fmt.Println(err)
-	}
+    fmt.Println(err)
+  }
   fmt.Printf("From func - checksum: %x\n", sum)
 
-	//Compress the file
+  //Compress the file
   bytes, err := fileop.Compress(filename)
   if err != nil {
-			fmt.Println(err)
-	}
-	fmt.Printf("From func - compress: %d\n", len(bytes))
+    fmt.Println(err)
+  }
+  fmt.Printf("From func - compress: %d\n", len(bytes))
 }
